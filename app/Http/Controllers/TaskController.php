d348b30d7ee94dd9ea51e6aa1ca8495456a90a35
gonchikovbb <gonchikovbb@gmail.com>
2023-06-14 21:26:13 +0300

    Fixed filter

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -90,24 +90,16 @@ class TaskController extends Controller
         $description = $request->input('description');
         $status = $request->input('status');
         $tag = $request->input('tag');
-        $arrTags = explode(' ', $tag);
 
-        $tasks = Task::query()
-            ->join('tags', 'tasks.id','=','tags.task_id')
-            ->select('tasks.*',)->distinct()
-            ->when($title, function($query, $title) {
+        $tasks = Task::query()->select('tasks.*',)->distinct()->when($title, function($query, $title) {
                 return $query->where('tasks.title', 'like', '%'.$title.'%');
-            })
-            ->when($description, function($query, $description) {
+            })->when($description, function($query, $description) {
                 return $query->where('tasks.description', 'like', '%'.$description.'%');
-            })
-            ->when($status, function($query, $status) {
+            })->when($status, function($query, $status) {
                 return $query->where('tasks.status', $status);
-            })
-            ->when($arrTags, function($query, $tags) {
-                return $query->whereIn('tags.name', $tags);
-            })
-            ->paginate();
+            })->when($tag, function($query, $tags) {
+                return $query->join('tags', 'tasks.id','=','tags.task_id')->whereIn('tags.name', $tags);
+            })->paginate();
 
         return view('tasks.showTasks', [
             'tasks' => $tasks,
